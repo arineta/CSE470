bdfe0b8bf49587f4a42752533e31f2fd8bde32d5
caghangir <caghangir@gmail.com>
2015-10-16 23:49:53 +0300

    homework3

---
+++ quote/CagatayDemirel.java
@@ -0,0 +1,17 @@
+package quote;
+
+public class CagatayDemirel implements Quotation {
+    public int year() {
+        return 1847;
+    }
+    public String text() {
+        String p = "We loved with a love that was more than love.";
+          return p;
+    }
+    public String author() {
+        return "Edgar Allan Poe";
+    }
+    public String reference() {
+        return "http://www.brainyquote.com/quotes/authors/e/edgar_allan_poe.html#C5dlP9OV6GrrO82y.99";
+    }
+}
