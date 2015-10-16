273fa8b167c81c16d693a412768ad60d772a552a
Nurullah <nurullah92@hotmail.com>
2015-10-16 17:29:05 +0300

    hmw3

---
+++ quote/NurullahAydoğdu.java
@@ -0,0 +1,30 @@
+package quote;
+
+
+public class NurullahAydogduimplements Quotation {
+
+    public int year() {
+
+        return 1860;
+    }
+  
+  public String text() {
+
+        String p = " What we think, or what we know, or what we believe is, in the end, of little consequence.\n";
+
+        String s = " The only consequence is what we do.";
+
+        return p+s;
+    }
+
+    public String author() {
+
+        return "Jhon Ruskin";
+    }
+
+    public String reference() {
+
+        return "https://litemind.com/best-famous-quotes/";
+    }
+
+}
