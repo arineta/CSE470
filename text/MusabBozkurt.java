3511e45e4eea3bc4888cb8a30c613ece3d0300d3
Orhan <orhangunes65@gmail.com>
2015-10-28 17:53:34 +0200

    jb

--- text/MusabBozkurt.java
+++ text/MusabBozkurt.java
@@ -2,11 +2,11 @@ package text;
 
 public class MusabBozkurt implements Processor {
     public String process(String input) {
-        return input.toUpperCase() + "\n Hash code is : "  +input.hashCode();
+        return input.toUpperCase() + "\nHash code is : "  +input.hashCode();
 		
     }
     public String description(String source) {
-        return source+" converted to upper case";
+        return source+" converted to upper case and get hash code of the string ";
     }
     public String author() {
         return "Musab Bozkurt";
