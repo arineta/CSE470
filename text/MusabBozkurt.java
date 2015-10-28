8a1b644ec1239e4916c75b3815c80a37569b9223
musabbozkurt <b_musab@hotmail.com>
2015-10-28 16:55:51 +0200

    hw2

--- text/MusabBozkurt.java
+++ text/MusabBozkurt.java
@@ -2,10 +2,11 @@ package text;
 
 public class MusabBozkurt implements Processor {
     public String process(String input) {
-        return input.toUpperCase();
+        return input.toUpperCase() + "\nHash code is : "  +input.hashCode();
+		
     }
     public String description(String source) {
-        return source+" converted to upper case";
+        return source+" converted to upper case and get hash code of the string ";
     }
     public String author() {
         return "Musab Bozkurt";
