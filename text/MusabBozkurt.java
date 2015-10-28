5d2c601385b79d22d81b16d3ede7a3ff5ab69300
musabbozkurt <b_musab@hotmail.com>
2015-10-28 16:36:51 +0200

    hw2

--- text/MusabBozkurt.java
+++ text/MusabBozkurt.java
@@ -2,10 +2,10 @@ package text;
 
 public class MusabBozkurt implements Processor {
     public String process(String input) {
-        return input.toLowerCase();
+        return input.toUpperCase();
     }
     public String description(String source) {
-        return source+" converted to lower case";
+        return source+" converted to upper case";
     }
     public String author() {
         return "Musab Bozkurt";
