57b48b91b989e39a4518056a0e1cc5edba6b9a39
Orhan <orhangunes65@gmail.com>
2015-10-28 17:59:20 +0200

    hw2 uppercase and length

--- text/OrhanGunes.java
+++ text/OrhanGunes.java
@@ -2,10 +2,10 @@ package text;
 
 public class OrhanGunes implements Processor {
     public String process(String input) {
-        return input.toUpperCase();
+        return input.toUpperCase() + "\nLength is : " + input.length();
     }
     public String description(String source) {
-        return source+" converted to upper case";
+        return source+" converted to upper case and get length of text";
     }
     public String author() {
         return "Orhan Gunes";
