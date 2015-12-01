1c86f505138201ae84f8a9b568c8192a8a6e5752
arineta xhembulla <arineta_xhembulla@yahoo.com>
2015-12-01 17:06:26 +0200

    changes

--- text/Arineta.java
+++ text/Arineta.java
@@ -1,13 +1,13 @@
 package text;
 
-public class Arineta implements Ari_processor {
+public class Arineta implements Processor {
     public String process(String input) {
         return input.toLowerCase();
     }
     public String description(String source) {
-        return source+" converted to lower case";
+        return source+" converted to UPPERCASE case";
     }
     public String author() {
-        return "arineta";
+        return "ARINETA";
     }
 }
