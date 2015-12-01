f9d1de90165941120f21f91e577f2426a37f6916
enesaytekin <enesaytekiin@hotmail.com>
2015-12-01 19:49:38 +0200

    A2
    Changed.

--- text/EnesAytekin.java
+++ text/EnesAytekin.java
@@ -1,13 +1,21 @@
 package text;
 
 public class EnesAytekin implements Processor {
-	 public String process(String input) {
-        return input.trim();
+    public String process(String input) {
+		
+		String newString="";
+		String newStringBigger="";
+		for(int i=0; i<input.length(); i++)
+       {
+          newString = newString + input.substring(i,i+1)+'*';
+	  newStringBigger = newString.UpperCase();
+       }	
+        return  newStringBigger+ "\nOldLength is : " + input.length()+"\nNewLength is :"+newStringBigger.length();
     }
     public String description(String source) {
-        return source+" converted to no blank space in the text.";
+        return source+" added '*' all words at the end and transformed bigger version of input.";
     }
     public String author() {
-        return "EnesAytekin";
+        return "Enes Aytekin";
     }
 }
